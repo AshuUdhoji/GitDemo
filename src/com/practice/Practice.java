46b5dcb4dd94e3edefa72accdd82b16cbcc316b7
ashutosh <ashutoshudhoji@gmail.com>
2020-12-13 22:26:04 +0530

    Commit from EST

--- src/com/practice/Practice.java
+++ src/com/practice/Practice.java
@@ -5,6 +5,9 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("Hello Switch!");
 		System.out.println("Hello 12LPA!");
+		
+		System.out.println("Hello Onsite!");
+		System.out.println("Hello Travel and Food!");
 	}
 
 }
