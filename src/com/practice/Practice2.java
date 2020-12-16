34f01f39ba490d893c698a9bf389d7322b6f498b
ashutosh <ashutoshudhoji@gmail.com>
2020-12-16 13:52:10 +0530

    Updating line in Practice2.java

--- src/com/practice/Practice2.java
+++ src/com/practice/Practice2.java
@@ -3,7 +3,7 @@ package com.practice;
 public class Practice2 {
 
 	public static void main(String[] args) {
-		System.out.println("Created nre java class");
+		System.out.println("Created new java class");
 
 	}
 
