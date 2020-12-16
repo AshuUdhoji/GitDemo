d0cb139a97d53da772fa003718a76a9da4ef3cc8
ashutosh <ashutoshudhoji@gmail.com>
2020-12-16 15:17:34 +0530

    Commit by develop

--- src/com/practice/Practice2.java
+++ src/com/practice/Practice2.java
@@ -4,6 +4,9 @@ public class Practice2 {
 
 	public static void main(String[] args) {
 		System.out.println("Created new java class");
+		
+		//Changes made in develop branch
+		System.out.println("New code added by dev branch");
 
 	}
 
